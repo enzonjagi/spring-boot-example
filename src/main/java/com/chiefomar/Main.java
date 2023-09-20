ca14d92ae22ad52ee8ff1660a9c77d007ccb5c91
enzonjagi <amosndungo@gmail.com>
2023-09-20 17:09:06 +0300

    Springboot Application Practice: Created a customer Model.

--- src/main/java/com/chiefomar/Main.java
+++ src/main/java/com/chiefomar/Main.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.geom.RectangularShape;
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -14,8 +17,52 @@ public class Main {
     }
     @GetMapping("/")
     public GreetResponse greet() {
-        return new GreetResponse("Hello");
+         GreetResponse response = new GreetResponse(
+                "Hello",
+                List.of("Java", "Python", "Kotlin"),
+                new Person(
+                        "Omar",
+                        28,
+                        900000.0
+                )
+         );
+
+        return response;
     }
 
-    record GreetResponse(String greet){}
+    record Person (String name, int age, double savings) {}
+    record GreetResponse(
+            String greet,
+            List<String>  favProgrammingLanguages,
+            Person person
+    ){}
+    /*NOTE: The above line of code can also be written as:
+    * class GreetResponse {
+    *   private final String greet;
+    *
+    *   GreetResponse (String greet) {
+    *       this.greet = greet;
+    *   }
+    *
+    *   @Override
+    *   public String toString() {
+    *       return "GreetResponse{" +
+    *               "greet="+ greet + '\'' + '}';
+    *   }
+    *
+    *   @Override
+    *   public boolean equals(Object o){
+    *       if (this == o) return true;
+    *       if (o == null || getClass() != o.getClass() ) return false;
+    *       GreetResponse that = (GreetResponse) o;
+    *       return Objects.equals(greet, that.greet);
+    *   }
+    *
+    *   @Override
+    *   public int hashCode() {
+    *       return Objects.hash(greet);
+    *   }
+    * }
+    * */
+
 }
